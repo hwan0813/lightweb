e0f5a16b94c6d37e6d5702798b732466cdc647a5
Jeong Seung Hwan <tmdghks5466@naver.com>
2017-09-07 18:27:16 +0900

    model수정+ 로컬도 mysql

--- lightweb/settings/common.py
+++ lightweb/settings/common.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'lightweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lightist',
+        'USER': 'root',
+        'PASSWORD': '0000',
+        'HOST':'',
+        'PORT':'',
     }
 }
 
--- post/models.py
+++ post/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class Post(models.Model):
+    
+    title = models.CharField(max_length=100, help_text='여기에 제목을 입력!!')
+    content = models.TextField(verbose_name="content",help_text='여기에 내용을 입력!!')
     image = models.ImageField()
+    
     created_at = models.DateTimeField(auto_now_add=True)
-
-    created_at2 = models.DateTimeField(auto_now_add=True)
+    

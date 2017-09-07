5005e784b9091a20f938d805602cd7aa12a3a668
Jeong Seung Hwan <tmdghks5466@naver.com>
2017-09-07 11:19:34 +0900

    before eb create

--- post/models.py
+++ post/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Post(models.Model):
     image = models.ImageField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    created_at2 = models.DateTimeField(auto_now_add=True)
